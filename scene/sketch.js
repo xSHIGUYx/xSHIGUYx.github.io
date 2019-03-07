f51f7374891e683a20e01f3d9ea7d1a0b82c6993
xSHIGUYx <47368990+xSHIGUYx@users.noreply.github.com>
2019-03-06 22:28:24 -0600

    Update sketch.js

--- scene/sketch.js
+++ scene/sketch.js
@@ -13,6 +13,8 @@ let verticalAcceleration, verticalSpeedMax, quickDown;
 let horizontalSpeed, horizontalSpeedMax, horizontalAcceleration;
 let isMovingRight, isMovingLeft, isMovingUp;
 let widthIncrease, widthDecrease;
+let heightIncrease, heightDecrease;
+let color, color2, color3, colorChange, colorBuffer;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -32,21 +34,42 @@ function setup() {
   isMovingUp = false;
   widthIncrease = false;
   widthDecrease = false;
+  heightIncrease = false;
+  heightDecrease = false;
   quickDown = false;
+  color = 255;
+  color2 = 255;
+  color3 = 255;
+  colorChange = false;
+  colorBuffer = true;
 }
 
 function draw() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  
-  rect(playerX, playerY, playerWidth, playerHeight);
-  console.log(playerY);
+  fill(255)
+  rect(0, windowHeight - 50, windowWidth, windowHeight / 10); ///DRAWS GROUND
+  if (colorChange && colorBuffer) {
+    colorBuffer = false;
+    color = random(255);
+    color2 = random(255);
+    color3 = random(255);
+  }
+  fill(color, color2, color3)
+  rect(playerX, playerY, playerWidth, playerHeight); ///DRAWS PLAYER
+  console.log(color);
 
   if (widthIncrease && playerWidth < 250 && playerX + playerWidth <= windowWidth) {
     playerWidth += 1;
   }
   if (widthDecrease && playerWidth > 20) {
     playerWidth += -1;
+  }
+  if (heightIncrease && playerHeight < 250 && playerY >= 0) {
+    playerHeight += 1;
+  }
+  if (heightDecrease && playerHeight > 20) {
+    playerHeight += -1;
   }
   
   ///Horizontal Calculations
@@ -78,9 +101,6 @@ function draw() {
   ///End Horizontal Calculations
 
   // Gravity
-  if (playerY < 0) {
-    playerY = 0;
-  }
 
   if (isMovingUp) {
     gravity = 0;
@@ -99,16 +119,21 @@ function draw() {
       horizontalSpeedMax = 6.0;
     }
   }
-  
   verticalSpeed += gravity;
-
-  if (playerY + playerHeight >= windowHeight - playerHeight && verticalSpeed >= 0 || playerY <= 0 && verticalSpeed < 0) {
+  if (playerY + playerHeight >= windowHeight - 50 && verticalSpeed >= 0 || playerY <= 0 && verticalSpeed < 0) {
     verticalSpeed = 0;
   }
-  playerY += verticalSpeed;
   if (Math.abs(verticalSpeed) > verticalSpeedMax) {
     verticalSpeed = verticalSpeedMax * sign(verticalSpeed);
   }
+  playerY += verticalSpeed;
+  if (playerY + playerHeight >= windowHeight - 50) {
+    playerY = windowHeight - 50 - playerHeight;
+  }
+  if (playerY <= 0) {
+    playerY = 0;
+  }
+
   //End Gravity
 }
 
@@ -122,14 +147,23 @@ function keyPressed() {
   if (keyCode === UP_ARROW) {
     isMovingUp = true;
   }
-  if (keyCode === DOWN_ARROW) {
+  if (keyCode === DOWN_ARROW) { 
     quickDown = true;
   }
-  if (keyCode === 119 || keyCode === 87) {
+  if (keyCode === 119 || keyCode === 87) { ///Key code for "W"
     widthIncrease = true;
   }
-  if (keyCode === 101 || keyCode === 69) {
+  if (keyCode === 101 || keyCode === 69) { ///Key code for "E"
     widthDecrease = true;
+  }
+  if (keyCode === 100 || keyCode === 68) { ///Key code for "D"
+    heightIncrease = true;
+  }
+  if (keyCode === 102 || keyCode === 70) { ///Key code for "F"
+    heightDecrease = true;
+  }
+  if (keyCode === 114 || keyCode === 82) { ///Key code for "R"
+    colorChange = true;
   }
 }
 
@@ -146,11 +180,21 @@ function keyReleased() {
   if (keyCode === DOWN_ARROW) {
     quickDown = false;
   }
-  if (keyCode === 119 || keyCode === 87) {
+  if (keyCode === 119 || keyCode === 87) { ///Key code for "W"
     widthIncrease = false;
   }
-  if (keyCode === 101 || keyCode === 69) {
+  if (keyCode === 101 || keyCode === 69) { ///Key code for "E"
     widthDecrease = false;
+  }
+  if (keyCode === 100 || keyCode === 68) { ///Key code for "D"
+    heightIncrease = false;
+  }
+  if (keyCode === 102 || keyCode === 70) { ///Key code for "F"
+    heightDecrease = false;
+  }
+  if (keyCode === 114 || keyCode === 82) { ///Key code for "R"
+    colorChange = false;
+    colorBuffer = true;
   }
 }
 
