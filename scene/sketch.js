2246a4a94f7c62e10c23e59eab6dc3ff0722bda2
xSHIGUYx <47368990+xSHIGUYx@users.noreply.github.com>
2019-03-06 21:46:13 -0600

    Update sketch.js

--- scene/sketch.js
+++ scene/sketch.js
@@ -9,15 +9,17 @@
 let playerX, playerY;
 let playerWidth, playerHeight;
 let gravity, verticalSpeed;
-let verticalAcceleration, verticalSpeedMax;
+let verticalAcceleration, verticalSpeedMax, quickDown;
 let horizontalSpeed, horizontalSpeedMax, horizontalAcceleration;
+let isMovingRight, isMovingLeft, isMovingUp;
+let widthIncrease, widthDecrease;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  playerX = 150;
+  playerX = windowWidth / 2;
   playerY = 0;
-  playerWidth = windowWidth / 37.5;
-  playerHeight = windowHeight / 10;
+  playerWidth = 35;
+  playerHeight = 50;
   gravity = 0.5;
   verticalSpeed = 0;
   verticalAcceleration = 0.35;
@@ -25,27 +27,37 @@ function setup() {
   horizontalSpeed = 0;
   horizontalSpeedMax = 6.0;
   horizontalAcceleration = 0.4;
+  isMovingLeft = false;
+  isMovingRight = false;
+  isMovingUp = false;
+  widthIncrease = false;
+  widthDecrease = false;
+  quickDown = false;
 }
 
 function draw() {
+  createCanvas(windowWidth, windowHeight);
   background(0);
   
   rect(playerX, playerY, playerWidth, playerHeight);
-  console.log("HSP: " + horizontalSpeed, "VSP: " + verticalSpeed);
-  console.log("GRV: " + gravity, "VSPACC: " + verticalAcceleration);
+  console.log(playerY);
+
+  if (widthIncrease && playerWidth < 250 && playerX + playerWidth <= windowWidth) {
+    playerWidth += 1;
+  }
+  if (widthDecrease && playerWidth > 20) {
+    playerWidth += -1;
+  }
   
   ///Horizontal Calculations
-  if (keyIsPressed && keyCode === RIGHT_ARROW || keyIsPressed && keyCode === LEFT_ARROW) {
-    if (keyCode === RIGHT_ARROW) {
-      horizontalSpeed += horizontalAcceleration;
-    }
-    if (keyCode === LEFT_ARROW) {
-      horizontalSpeed += -horizontalAcceleration;
-    }
-  } else {
-    if (horizontalSpeed !== 0) {
-      horizontalSpeed += horizontalAcceleration * -sign(horizontalSpeed);
-    }
+  if (isMovingRight) {
+    horizontalSpeed += horizontalAcceleration;
+  }
+  if (isMovingLeft) {
+    horizontalSpeed += -horizontalAcceleration;
+  }
+  if (horizontalSpeed !== 0 && !isMovingRight && !isMovingLeft) {
+    horizontalSpeed += horizontalAcceleration * -sign(horizontalSpeed);
     if (horizontalSpeed < 0.5 && horizontalSpeed > -0.5) {
       horizontalSpeed = 0;
     }
@@ -56,27 +68,41 @@ function draw() {
   }
   playerX += horizontalSpeed;
   if (playerX <= 0) {
-    playerX = 0;
+    horizontalSpeed = 0;
+    isMovingLeft = false;
   }
   if (playerX + playerWidth >= windowWidth) {
-    playerX = windowWidth - playerWidth;
+    horizontalSpeed = 0;
+    isMovingRight = false;
   }
   ///End Horizontal Calculations
 
   // Gravity
-  if (keyIsPressed && keyCode === UP_ARROW) {
+  if (playerY < 0) {
+    playerY = 0;
+  }
+
+  if (isMovingUp) {
     gravity = 0;
     verticalSpeed += -verticalAcceleration;
   } else {
-    gravity = 0.5;
+    if (quickDown) {
+      gravity = 0.9;
+      verticalSpeedMax = 75;
+      horizontalSpeedMax = 1.0;
+      if (sign(verticalSpeed) === -1) {
+        verticalSpeed *= -1;
+      }
+    } else {
+      gravity = 0.5;
+      verticalSpeedMax = 25;
+      horizontalSpeedMax = 6.0;
+    }
   }
   
   verticalSpeed += gravity;
 
-  if (playerY <= 0) {
-    playerY = 0;
-  }
-  if (playerY + playerHeight >= windowHeight - playerHeight && keyCode !== UP_ARROW) {
+  if (playerY + playerHeight >= windowHeight - playerHeight && verticalSpeed >= 0 || playerY <= 0 && verticalSpeed < 0) {
     verticalSpeed = 0;
   }
   playerY += verticalSpeed;
@@ -86,9 +112,45 @@ function draw() {
   //End Gravity
 }
 
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW) {
+    isMovingRight = true;
+  }
+  if (keyCode === LEFT_ARROW) {
+    isMovingLeft = true;
+  }
+  if (keyCode === UP_ARROW) {
+    isMovingUp = true;
+  }
+  if (keyCode === DOWN_ARROW) {
+    quickDown = true;
+  }
+  if (keyCode === 119 || keyCode === 87) {
+    widthIncrease = true;
+  }
+  if (keyCode === 101 || keyCode === 69) {
+    widthDecrease = true;
+  }
+}
+
 function keyReleased() {
-  if (keyCode !== RIGHT_ARROW || keyCode !== LEFT_ARROW) { //|| keyCode !== UP_ARROW || keyCode !== DOWN_ARROW) {
-    keyCode = "";
+  if (keyCode === RIGHT_ARROW) {
+    isMovingRight = false;
+  }
+  if (keyCode === LEFT_ARROW) {
+    isMovingLeft = false;
+  }
+  if (keyCode === UP_ARROW) {
+    isMovingUp = false;
+  }
+  if (keyCode === DOWN_ARROW) {
+    quickDown = false;
+  }
+  if (keyCode === 119 || keyCode === 87) {
+    widthIncrease = false;
+  }
+  if (keyCode === 101 || keyCode === 69) {
+    widthDecrease = false;
   }
 }
 
