2ce49305132ca9a86412fa4df936650a36ae52e7
xSHIGUYx <shilohberscheid@gmail.com>
2019-04-09 14:26:40 -0600

    Update sketch.js

--- grid-project/sketch.js
+++ grid-project/sketch.js
@@ -32,9 +32,15 @@ function create2DArray(cols, rows) {
   for (let i = 0; i < rows; i++) {
     emptyArray.push([]);
     for (let j = 0; j < cols; j++) {
+      ///Player create
       if (j === 3 && i === 2) {
         emptyArray[i].push(1);
       }
+      ///Wall create
+      if (j !== 3 && i === 4) {
+        emptyArray[i].push(2);
+      }
+      ///Blank Space Create
       else {
         emptyArray[i].push(0);
       }
@@ -46,8 +52,13 @@ function create2DArray(cols, rows) {
 function displayGrid() {
   for (let y = 0; y < gridSize; y++) {
     for (let x = 0; x < gridSize; x++) {
-      if (grid[y][x] === 0) {
-        fill(255);
+      if (grid[y][x] === 0 || grid[y][x] === 2) {
+        if (grid[y][x] === 2) {
+          fill(255, 0, 0);
+        }
+        else {
+          fill(255);
+        }
       }
       else {
         fill(0);
@@ -58,10 +69,61 @@ function displayGrid() {
 }
 
 function keyPressed() {
-  if (key === "d") {
+  ///Leftwards Movement
+  if (key === "a" || key === "A") {
     for (let y = 0; y < gridSize; y++) {
       for (let x = 0; x < gridSize; x++) {
-        
+        if (grid[y][x] === 1) {
+          if (grid[y][x - 1] === 0) {
+            grid[y][x] = 0;
+            grid[y][x - 1] = 1;
+          }
+        }
+      }
+    }
+  }
+  ///Rightwards Movement
+  if (key === "d" || key === "D") {
+    for (let y = 0; y < gridSize; y++) {
+      for (let x = 0; x < gridSize; x++) {
+        if (grid[y][x] === 1) {
+          if (grid[y][x + 1] === 0) {
+            grid[y][x] = 0;
+            grid[y][x + 1] = 1;
+            break;
+          }
+        }
+      }
+    }
+  }
+  ///Upwards Movement
+  if (key === "w" || key === "W") {
+    for (let y = 0; y < gridSize; y++) {
+      for (let x = 0; x < gridSize; x++) {
+        if (grid[y][x] === 1) {
+          if (grid[y - 1][x] === 0) {
+            grid[y][x] = 0;
+            grid[y - 1][x] = 1;
+          }
+        }
+      }
+    }
+  }
+  ///Downwards Movement
+  if (key === "s" || key === "S") {
+    for (let y = 0; y < gridSize; y++) {
+      let counter = 0;
+      for (let x = 0; x < gridSize; x++) {
+        if (grid[y][x] === 1) {
+          if (grid[y + 1][x] === 0) {
+            grid[y][x] = 0;
+            grid[y + 1][x] = 1;
+            counter = 1;
+          }
+        }
+      }
+      if (counter === 1) {
+        break;
       }
     }
   }
